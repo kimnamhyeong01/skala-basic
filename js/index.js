255e068aabc07d1908142fb82f86695bf1da616a
Kim Nam Hyeong <luckynhkim@naver.com>
2026-01-20 18:16:25 +0900

    Javascript-이상민교수님

---
+++ js/index.js
@@ -0,0 +1,6 @@
+console.log("Hello, world!"); 
+console.error("This is an error message."); 
+document.write("<p>This is written using document.write()</p>"); 
+document.write("아무 문장이나 작성");
+document.write("추가된 문장")
+alert("알림 메시지")
